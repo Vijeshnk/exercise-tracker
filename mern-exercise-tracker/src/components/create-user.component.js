df995056c87c231e923dc54610e928e024ea6fcf
vijeshnk <vijeshnirala@gmail.com>
2020-10-01 15:35:31 +0530

    connect backend to frontend

--- mern-exercise-tracker/src/components/create-user.component.js
+++ mern-exercise-tracker/src/components/create-user.component.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 export default class CreateUsers extends Component {
 
@@ -30,6 +31,11 @@ onSubmit(e){
     }
 
     console.log(user);
+
+    axios.post('http://localhost:5000/users/add', user)
+        .then(res => console.log(res.data));
+
+
 
    this.setState({
        username:''
